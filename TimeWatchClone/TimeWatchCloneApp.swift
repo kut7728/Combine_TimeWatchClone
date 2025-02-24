4ecf640c8063ca0a6a8fdf2ba9d245bd912482d9
kut7728 <kut7728@hanyang.ac.kr>
2025-02-24 15:22:04 +0900

    Initial Commit

---
+++ TimeWatchClone/TimeWatchCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeWatchCloneApp.swift
+//  TimeWatchClone
+//
+//  Created by nelime on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeWatchCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
